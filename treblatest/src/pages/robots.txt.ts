7d0584196892b5c0a3c045911779383a726b04b7
Trebla Studio <trebla.dev.simoni@gmail.com>
2026-09-11 23:13:46 +0200

    chore: rimuove rebuild da TreblaStudio, ora vive nella cartella indipendente TreblaTest

--- treblatest/src/pages/robots.txt.ts
+++
@@ -1,7 +0,0 @@
-import { site } from '../data/site';
-
-export function GET() {
-  const base = site.url.replace(/\/$/, '');
-  const txt = `User-agent: *\nAllow: /\n\nSitemap: ${base}/sitemap.xml\n`;
-  return new Response(txt, { headers: { 'Content-Type': 'text/plain; charset=utf-8' } });
-}
